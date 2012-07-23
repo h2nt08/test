57e93c04c5397c993da933e9841f9ddb4348b2b6
vuna2 <vuna2@exoplatform.com>
2012-07-23 09:48:57 +0700

    xxxxx

--- eXo-automation/shScript/sniff_suite_bk.sh
+++ eXo-automation/shScript/sniff_suite_bk.sh
@@ -7,7 +7,7 @@ $JAVA_HOME/bin/java -jar $HOME_TEST/selenium-server-standalone-2.21.0.jar -userE
 $JAVA_HOME/bin/java -jar $HOME_TEST/selenium-server-standalone-2.21.0.jar -userExtensions $HOME_TEST/jsScript/user-extensions.js -htmlsuite "*firefox" "http://localhost:8080" $HOME_TEST/htmlscript/ecms/Functional/Content_Explorer/ecms_fe_basic_action.html $HOME_TEST/Reports/ecms_fe_basic_action_report.html
 
 exit  0
-#echo "=========check return code========"
+#echo "=========check return code======="
 #PROC_RET=$?
 #echo "Returning $PROC_RET to Qmetry"
 #echo "Returning $PROC_RET to Qmetry" >> /home/hainh/java/eXoProjects/test/eXo-automation/Reports/Debug.log
