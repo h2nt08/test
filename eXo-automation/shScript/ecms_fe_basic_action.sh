6141d6dc0666a466cae5984828bbd5d64a04f2f6
hainh <hainh@exoplatform.com>
2012-07-23 08:34:30 +0700

    add sh script

--- eXo-automation/shScript/ecms_fe_basic_action.sh
+++ eXo-automation/shScript/ecms_fe_basic_action.sh
@@ -1,2 +1,5 @@
-export HOME=/home/hainh/java/eXoProjects/test/eXo-automation
-java -jar $HOME/selenium-server-standalone-2.21.0.jar -userExtensions $HOME/jsScript/user-extensions.js -htmlsuite "*firefox" "http://localhost:8080" $HOME/htmlscript/ecms/Snifftest/sniff_suite.html $HOME/Reports/ecms_fe_basic_action_report.html
+export HOME_TEST="/home/SELENIUM-PLF3.5.X/workspace/DownloadECMSAutomationProjects/eXo-automation"
+cd $HOME_TEST
+export ReportName=ecms_fe_basic_action_$(date +"%m-%d-%Y").html
+pwd
+java -jar selenium-server-standalone-2.21.0.jar -userExtensions $HOME_TEST/jsScript/user-extensions.js -htmlsuite "*firefox" "http://localhost:8080" $HOME_TEST/htmlscript/ecms/Functional/Content_Explorer/ecms_fe_basic_action.html $HOME_TEST/Reports/$ReportName
---
+++ eXo-automation/shScript/test.sh
@@ -0,0 +1,7 @@
+/home/SELENIUM-PLF3.5.X/workspace/shScript/ecms_fe_admin.sh
+echo "==========================================="
+/home/SELENIUM-PLF3.5.X/workspace/shScript/ecms_fe_info.sh
+
+/home/SELENIUM-PLF3.5.X/workspace/shScript/ecms_fe_basic_action.sh
+/home/SELENIUM-PLF3.5.X/workspace/shScript/ecms_fe_collaboration.sh
+
